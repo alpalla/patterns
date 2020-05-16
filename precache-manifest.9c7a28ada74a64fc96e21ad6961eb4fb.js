5f438ac529f9dbb42a04697e105cf5c3be55cdde
Al <46252231+alpalla@users.noreply.github.com>
2020-05-16 14:07:47 +0200

    Updates

---
+++ precache-manifest.9c7a28ada74a64fc96e21ad6961eb4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847b5f219074b9a7d29fed47406f34fb",
+    "url": "/patterns/index.html"
+  },
+  {
+    "revision": "b29d55673d8c73931fe8",
+    "url": "/patterns/static/css/main.e9fcfc63.chunk.css"
+  },
+  {
+    "revision": "b6e2f8e160fef0af70c5",
+    "url": "/patterns/static/js/2.01760fe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/patterns/static/js/2.01760fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29d55673d8c73931fe8",
+    "url": "/patterns/static/js/main.5faaf0d2.chunk.js"
+  },
+  {
+    "revision": "84069f5ae6b61cbc53eb",
+    "url": "/patterns/static/js/runtime-main.bd1fac23.js"
+  }
+]);
